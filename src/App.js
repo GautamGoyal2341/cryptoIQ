3ad13be3305d6f3ff1b0529c37cf7ec90749c661
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-11-02 11:09:16 +0530

    route setup done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router , Routes , Route  } from 'react-router-dom'
-import Header from './components/header'
+import { BrowserRouter as Router , Routes , Route  } from 'react-router-dom';
+import Header from './components/header';
+import Home from  "./components/Home";
+import Coins from './components/Coins';
+import Exchanges from './components/Exchanges';
+import CoinDetails from './components/CoinDetails';
 function App()
 {
 
@@ -12,7 +16,11 @@ function App()
        <Header/>
             <Routes>
 
-            <Route/>
+            <Route  path='/' element = {<Home/>}/>
+            <Route  path='/coins' element = {<Coins/>}/>
+            <Route  path='/exchanges' element = {<Exchanges/>}/>
+            <Route  path='/coind/:id' element = {<CoinDetails/>}/>
+      
 
             </Routes>
 
