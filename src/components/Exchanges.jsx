9c2fc4d4d39ae403d2b6782afea329845427110f
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-11-03 11:50:01 +0530

    image with css code done

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,50 +1,51 @@
-import React , {useEffect , useState} from 'react'
-import axios from 'axios'
-import { server } from '../index'
-import { Container, HStack } from '@chakra-ui/react';
-import Loader from './Loader';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { server } from "../index";
+import { Container, HStack, VStack } from "@chakra-ui/react";
+import Loader from "./Loader";
 const Exchanges = () => {
+  const [exchanges, setExchanges] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-     const [exchanges , setExchanges] = useState([]);
-     const [loading , setLoading] = useState(true); 
+  useEffect(() => {
+    const fetchExchanges = async () => {
+      const { data } = await axios.get(`${server}/exchanges`);
+      setExchanges(data);
+      setLoading(false);
+    };
 
-      useEffect(() => {
-
-        const fetchExchanges = async()=>{
-            const {data} =  await axios.get(`${server}/exchanges`)
-            setExchanges(data)
-            setLoading(false);
-        }
-
-             
     fetchExchanges();
-        
-      },[])
+  }, []);
 
-  return <Container maxW = {"container.xl"}>
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <HStack wrap={"wrap"}>
+            {exchanges.map((i) => (
+              <ExchangeCard
+                key={i.id}
+                name={i.name}
+                img={i.image}
+                rank={i.trust_score_rank}
+                url={i.url}
+              />
+            ))}
+          </HStack>
+        </>
+      )}
+    </Container>
+  );
+};
 
-    {loading ? <Loader/> :  <> 
-      
-       <HStack wrap={"wrap"}>
-  
-           {
-            exchanges.map((i)=>(
-                <ExchangeCard/>
-            ))
-           }
-
-
-
-       </HStack>
-    
-    
-    </>}
-     
-  </Container>
-}
-
-const ExchangeCard  = () => (
-    
-)
+const ExchangeCard = ({ name, img, rank, url }) => (
+  <a href={url} target={"blank"}>
+    <VStack>
+      <Image src={img} w={"10"} h={"10"} objectFit={"contain"} />
+    </VStack>
+  </a>
+);
 
 export default Exchanges;
