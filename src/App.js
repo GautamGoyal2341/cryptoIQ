d830c175f9da5a97ee69ad12e506b7c80f9341cf
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-11-04 14:56:31 +0530

    image error resolcved

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
-import React from 'react'
-import { BrowserRouter as Router , Routes , Route  } from 'react-router-dom';
-import Header from './components/Header';
-import Home from  "./components/Home";
-import Coins from './components/Coins';
-import Exchanges from './components/Exchanges';
-import CoinDetails from './components/CoinDetails';
-function App()
-{
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Coins from "./components/Coins";
+import Exchanges from "./components/Exchanges";
+import CoinDetails from "./components/CoinDetails";
+;
 
-    return(
-        
-        <Router>
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coins" element={<Coins />} />
+        <Route path="/exchanges" element={<Exchanges />} />
+        <Route path="/coin/:id" element={<CoinDetails />} />
+      </Routes>
 
-            
-       <Header/>
-            <Routes>
-
-            <Route  path='/' element = {<Home/>}/>
-            <Route  path='/coins' element = {<Coins/>}/>
-            <Route  path='/exchanges' element = {<Exchanges/>}/>
-            <Route  path='/coind/:id' element = {<CoinDetails/>}/>
-      
-
-            </Routes>
+   
 
 
-        </Router>
- 
-
-        )
-
+      <div>ji</div>
+    </Router>
+  );
 }
 
-
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
-import ReactDOM  from "react-dom";
-import {ChakraProvider,theme} from "@chakra-ui/react"
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 
-import App from './App';
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <ChakraProvider theme={theme}>
-    <App/>
-    </ChakraProvider>,
-    document.getElementById('root')
-)
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
 
-export const server = `https://api.coingecko.com/api/v3`
+export const server = `https://api.coingecko.com/api/v3`;
