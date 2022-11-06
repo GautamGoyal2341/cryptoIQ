9949af65cbfc1b2bfc158442bbb8930768564f76
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-11-06 15:21:10 +0530

    error component changed

--- src/components/ErrorComponent.jsx
+++ src/components/ErrorComponent.jsx
@@ -1,20 +1,9 @@
-import { Alert, AlertIcon } from "@chakra-ui/react";
-import React from "react";
+import React from 'react'
 
-const ErrorComponent = ({ message }) => {
+const ErrorComponent = () => {
   return (
-    <Alert
-      status="error"
-      position={"fixed"}
-      bottom={"4"}
-      left={"50%"}
-      transform={"translateX(-50%)"}
-      w={"container.lg"}
-    >
-      <AlertIcon />
-      {message}
-    </Alert>
-  );
-};
+    <div>ErrorComponent</div>
+  )
+}
 
-export default ErrorComponent;
+export default ErrorComponent
