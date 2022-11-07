21f5b5518e5709c5e17f935a02df7f1dc30e6e76
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-11-07 15:30:40 +0530

    coin det. half done with loader errer

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,9 +1,26 @@
 import { Container } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Loader from "./Loader"
 
 const CoinDetails = () => {
-  return <Container  >
 
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("inr");
+
+    
+  return <Container maxW = {"container.xl"}  >
+
+ 
+  {
+    loading?<Loader/>:<>
+    
+    
+    
+    </>
+  }
 
 
 
