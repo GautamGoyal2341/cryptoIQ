559a12171c02ce7e424d2c273a20297d1aeb551e
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-11-08 15:52:33 +0530

    image and current time

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,6 +1,7 @@
-import { Container } from '@chakra-ui/react'
+import { Container } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import Loader from "./Loader"
+import Loader from "./Loader";
+import { useParams } from "react-router-dom";
 import {
   Badge,
   Box,
@@ -18,25 +19,23 @@ import {
   StatNumber,
   Text,
   VStack,
-} from "@chakra-ui/react"
+} from "@chakra-ui/react";
+import { server } from "..index";
 
 const CoinDetails = () => {
-
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [page, setPage] = useState(1);
   const [currency, setCurrency] = useState("inr");
 
-
+  const params = useParams();
 
   useEffect(() => {
     const fetchCoin = async () => {
       try {
-        const { data } = await axios.get(
-          `${server}/coins/markets?vs_currency=${currency}&page=${page}`
-        );
-        setCoins(data);
+        const { data } = await axios.get(`${server}/coins/{id}`);
+        setCoin(data);
         setLoading(false);
       } catch (error) {
         setError(true);
@@ -44,25 +43,46 @@ const CoinDetails = () => {
       }
     };
     fetchCoin();
-  }, [currency, page]);
+  }, [params.id]);
 
-    
-  return <Container maxW = {"container.xl"}  >
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Box width={"full"} borderwidth={1}>
+            afdss
+          </Box>
+          <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
+            <HStack spacing={"4"}>
+              <Radio value={"inr"}>INR</Radio>
+              <Radio value={"usd"}>USD</Radio>
+              <Radio value={"eur"}>EUR</Radio>
+            </HStack>
+          </RadioGroup>
 
- 
-  {
-    loading?<Loader/>:<>
-    
-    <Box width = {"full"} borderwidth = {1}>
-      afdss
-    </Box>
-     
-    </>
-  }
+          <VStack spacing={"4"} p={"16"} alignItems={"flex-start"}>
+            <Text fontSize={"small"} alignSelf="center" opacity={0.7}>
+              Last Updated On{" "}
+              {Date(coin.market_data.last_updated).split("G")[0]}
+            </Text>
 
+            <Image
+              src={coins.image.large}
+              w={"16"}
+              h={"16"}
+              objectFit={"contain"}
+            />
+            <Stat>
+              <StatLabel>{coin.name}</StatLabel>
+              <StatNumber></StatNumber>
+            </Stat>
+          </VStack>
+        </>
+      )}
+    </Container>
+  );
+};
 
-
-  </Container>
-}
-
-export default CoinDetails
+export default CoinDetails;
