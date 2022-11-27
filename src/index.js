558566801e39cc5c5c0a651747242627d3d5e73d
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-11-27 11:26:54 +0530

    exchange api setup done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM  from "react-dom";
-import {ChakraProvider} from "@chakra-ui/react"
+import {ChakraProvider,theme} from "@chakra-ui/react"
 
 import App from './App';
 
 ReactDOM.render(
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
     <App/>
     </ChakraProvider>,
     document.getElementById('root')
 )
+
+export const server = `https://api.coingecko.com/api/v3`
